8d7f030a41f7ac0b294a8f3ca2a6a4e36c328fca
root <root@cicdprojectsrv1.radical.com>
2026-07-28 11:58:59 +0530

    Added CI/CD Dashboard

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Show dashboard
 app.get('/', (req, res) => {
-    res.send('DevOps CI/CD Project Working');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(3000, () => {
-    console.log('Running on port 3000');
+app.listen(3000, '0.0.0.0', () => {
+    console.log("Running on port 3000");
 });
